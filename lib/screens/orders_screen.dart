d366e116e972ee0df08ac95e4eb74f53ba4859db
Nayra Khaled <sci.nayerakhaled2018@alexu.edu.eg>
2021-06-10 18:14:56 +0200

    orders

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop_app/widgets/order_item.dart';
+import '../providers/orders.dart' show Orders;
 
 class OrdersScreen extends StatelessWidget {
   static const routeName = "/orders";
@@ -8,11 +12,32 @@ class OrdersScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Shop"),
-      ),
-      body: Center(
+        title: Text("Your Order"),
       ),
       drawer: AppDrawer(),
+      body: FutureBuilder(
+        future:
+            Provider.of<Orders>(context, listen: false).fetchAndSetProducts(),
+        builder: (ctx, snapShot) {
+          if (snapShot.connectionState == ConnectionState.waiting)
+            return CircularProgressIndicator();
+          else {
+            if (snapShot.error != null) {
+              return Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (context, index) =>
+                      OrderItem(orderData.orders[index]),
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
